840547c8019ef7545f743649e7b730fca55207b6
Saurab-Negi <saurabnegi17@gmail.com>
2024-07-30 17:22:04 +0530

    Code updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
-export default function App() {
+import './App.css'
+import { Pokemon } from './Pokemon'
+
+const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+      <Pokemon/>
+    </div>
   )
 }
+
+export default App
